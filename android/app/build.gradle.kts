cc677950c9c44f0abbff9cef5f2f35102153b54b
coderdy-git <coderdy-git@users.noreply.github.com>
2026-06-27 19:11:05 +0700

    chore: fix package name typo (notes)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.officeboy.office_boy_app"
+    namespace = "com.coderdy.officeboy_notes"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.coderdy.officeboy_note"
+        applicationId = "com.coderdy.officeboy_notes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
